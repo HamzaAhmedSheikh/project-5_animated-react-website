a0b9276f0de92b4569bb54d70d043d5a4604eb93
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-08-19 03:08:12 +0500

    add all images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+import Navbar from './Animated-React-Website/Navbar'
 
 function App() {
   return (
-    <div className="App">
-       <h1> Hello World </h1>      
+    <div>
+       <Navbar />
     </div>
   );
 }
